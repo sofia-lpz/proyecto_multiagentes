d825224490a56e99478cd6665bc37f50f11b56a4
sofia-lpz <a01028251@tec.mx>
2024-11-27 00:34:01 -0600

    created roads under the destinations

--- Server/trafficBase/model.py
+++ Server/trafficBase/model.py
@@ -1,24 +1,14 @@
 from mesa import Model
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
-from .agent import *
+from agent import *
 import json
 
 class CityModel(Model):
-    """ 
-        Creates a model based on a city map.
-
-        Args:
-            N: Number of agents in the simulation
-    """
     def __init__(self, N):
-
-        # Load the map dictionary. The dictionary maps the characters in the map file to the corresponding agent.
         dataDictionary = json.load(open("city_files/mapDictionary.json"))
-
         self.traffic_lights = []
 
-        # Load the map file. The map file is a text file where each character represents an agent.
         with open('city_files/2022_base.txt') as baseFile:
             lines = baseFile.readlines()
             self.width = len(lines[0])-1
@@ -35,24 +25,21 @@ class CityModel(Model):
                         self.grid.place_agent(agent, (c, self.height - r - 1))
 
                     elif col in ["S", "s"]:
-                        # Create traffic light
                         agent = Traffic_Light(f"tl_{r*self.width+c}", self, False if col == "S" else True, int(dataDictionary[col]))
                         self.grid.place_agent(agent, (c, self.height - r - 1))
                         self.schedule.add(agent)
                         self.traffic_lights.append(agent)
                         
-                        # Find road direction by checking neighboring cells
                         road_direction = None
                         if r > 0 and lines[r-1][c] in ["v", "^"]:
-                            road_direction = "Up" if lines[r-1][c] == "^" else "Down"
+                            road_direction = dataDictionary[lines[r-1][c]]
                         elif r < len(lines)-1 and lines[r+1][c] in ["v", "^"]:
-                            road_direction = "Up" if lines[r+1][c] == "^" else "Down"
+                            road_direction = dataDictionary[lines[r+1][c]]
                         elif c > 0 and lines[r][c-1] in [">", "<"]:
-                            road_direction = "Right" if lines[r][c-1] == ">" else "Left"
+                            road_direction = dataDictionary[lines[r][c-1]]
                         elif c < len(lines[r])-1 and lines[r][c+1] in [">", "<"]:
-                            road_direction = "Right" if lines[r][c+1] == ">" else "Left"
+                            road_direction = dataDictionary[lines[r][c+1]]
                         
-                        # Create road under traffic light
                         if road_direction:
                             road = Road(f"r_{r*self.width+c}", self, road_direction)
                             self.grid.place_agent(road, (c, self.height - r - 1))
@@ -64,28 +51,61 @@ class CityModel(Model):
                     elif col == "D":
                         agent = Destination(f"d_{r*self.width+c}", self)
                         self.grid.place_agent(agent, (c, self.height - r - 1))
+                        
+                        print(f"\nChecking destination at position ({r}, {c})")
+                        road_direction = None
+                        
+                        # Only look for road symbols, not obstacles
+                        road_symbols = ["v", "^", ">", "<"]
+                        
+                        # Check above
+                        if r > 0 and lines[r-1][c] in road_symbols:
+                            print(f"Above cell contains: {lines[r-1][c]}")
+                            road_direction = dataDictionary[lines[r-1][c]]
+                            print(f"Found road above, direction: {road_direction}")
+                        
+                        # Check below
+                        elif r < len(lines)-1 and lines[r+1][c] in road_symbols:
+                            print(f"Below cell contains: {lines[r+1][c]}")
+                            road_direction = dataDictionary[lines[r+1][c]]
+                            print(f"Found road below, direction: {road_direction}")
+                        
+                        # Check left
+                        elif c > 0 and lines[r][c-1] in road_symbols:
+                            print(f"Left cell contains: {lines[r][c-1]}")
+                            road_direction = dataDictionary[lines[r][c-1]]
+                            print(f"Found road to left, direction: {road_direction}")
+                        
+                        # Check right
+                        elif c < len(lines[r])-1 and lines[r][c+1] in road_symbols:
+                            print(f"Right cell contains: {lines[r][c+1]}")
+                            road_direction = dataDictionary[lines[r][c+1]]
+                            print(f"Found road to right, direction: {road_direction}")
+                                
+                        if road_direction is None:
+                            print("No adjacent roads found, defaulting to Right")
+                            road_direction = "Right"
+                        
+                        print(f"Final road direction: {road_direction}")
+                        
+                        road = Road(f"r_{r*self.width+c}", self, road_direction)
+                        self.grid.place_agent(road, (c, self.height - r - 1))
 
         self.num_agents = N
         self.running = True
 
-        # First get all destinations from the grid
         destinations = []
         for cell in self.grid.coord_iter():
-            cell_content = cell[0]  # cell is a tuple of (content, x, y)
+            cell_content = cell[0]
             for agent in cell_content:
                 if isinstance(agent, Destination):
                     destinations.append(agent)
 
-
-        if destinations:  # Make sure there are destinations
-            # Pick a random destination
+        if destinations:
             destination = self.random.choice(destinations)
-            
-            # Create car with destination
             car = Car(f"car_0", self, destination)
             self.grid.place_agent(car, (0, 0))
             self.schedule.add(car)
 
     def step(self):
-        '''Advance the model by one step.'''
         self.schedule.step()
