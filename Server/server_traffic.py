99563824b7313481acf40a6e684bf0f22f1481ce
sofia-lpz <a01028251@tec.mx>
2024-11-26 23:37:18 -0600

    server stuff

--- Server/server_traffic.py
+++ Server/server_traffic.py
@@ -1,12 +1,11 @@
 # TC2008B. Sistemas Multiagentes y Gráficas Computacionales
-# Python flask server to interact with Unity. Based on the code provided by Sergio Ruiz.
-# Octavio Navarro. October 2023 
+# Python flask server to interact with webGL.
+# Octavio Navarro. 2024
 
 from flask import Flask, request, jsonify
-#from randomAgents.model import RandomModel
-#from randomAgents.agent import RandomAgent, ObstacleAgent
-from trafficBase.model import CityModel
-from trafficBase.agent import Road, Traffic_Light, Obstacle, Destination
+from flask_cors import CORS, cross_origin
+from randomAgents.model import RandomModel
+from randomAgents.agent import RandomAgent, ObstacleAgent
 
 # Size of the board:
 number_agents = 10
@@ -15,61 +14,95 @@ height = 28
 randomModel = None
 currentStep = 0
 
-# This application will be used to interact with Unity
+# This application will be used to interact with WebGL
 app = Flask("Traffic example")
+cors = CORS(app, origins=['http://localhost'])
 
 # This route will be used to send the parameters of the simulation to the server.
-# The servers expects a POST request with the parameters in a form.
+# The servers expects a POST request with the parameters in a.json.
 @app.route('/init', methods=['POST'])
+@cross_origin()
 def initModel():
     global currentStep, randomModel, number_agents, width, height
 
     if request.method == 'POST':
-        number_agents = int(request.form.get('NAgents'))
-        currentStep = 0
+        try:
 
-        print(request.form)
+            number_agents = int(request.json.get('NAgents'))
+            width = int(request.json.get('width'))
+            height = int(request.json.get('height'))
+            currentStep = 0
 
-        # Create the model using the parameters sent by Unity
-        randomModel = CityModel(number_agents)
+            print(request.json)
+            print(f"Model parameters:{number_agents, width, height}")
 
-        # Return a message to Unity saying that the model was created successfully
-        return jsonify({"message":"Parameters recieved, model initiated."})
+            # Create the model using the parameters sent by the application
+            randomModel = RandomModel(number_agents, width, height)
+
+            # Return a message to saying that the model was created successfully
+            return jsonify({"message":"Parameters recieved, model initiated."})
+
+        except Exception as e:
+            print(e)
+            return jsonify({"message":"Erorr initializing the model"}), 500
 
 # This route will be used to get the positions of the agents
-#@app.route('/getAgents', methods=['GET'])
-#def getAgents():
-    #global randomModel
+@app.route('/getAgents', methods=['GET'])
+@cross_origin()
+def getAgents():
+    global randomModel
 
-    #if request.method == 'GET':
-        ## Get the positions of the agents and return them to Unity in JSON format.
-        ## Note that the positions are sent as a list of dictionaries, where each dictionary has the id and position of an agent.
-        ## The y coordinate is set to 1, since the agents are in a 3D world. The z coordinate corresponds to the row (y coordinate) of the grid in mesa.
-        #agentPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for a, (x, z) in randomModel.grid.coord_iter() if isinstance(a, RandomAgent)]
+    if request.method == 'GET':
+        # Get the positions of the agents and return them to WebGL in JSON.json.t.
+        # Note that the positions are sent as a list of dictionaries, where each dictionary has the id and position of an agent.
+        # The y coordinate is set to 1, since the agents are in a 3D world. The z coordinate corresponds to the row (y coordinate) of the grid in mesa.
+        try:
+            agentPositions = [
+                {"id": str(a.unique_id), "x": x, "y":1, "z":z}
+                for a, (x, z) in randomModel.grid.coord_iter()
+                if isinstance(a, RandomAgent)
+            ]
 
-        #return jsonify({'positions':agentPositions})
+            return jsonify({'positions':agentPositions})
+        except Exception as e:
+            print(e)
+            return jsonify({"message":"Error with the agent positions"}), 500
 
-## This route will be used to get the positions of the obstacles
-#@app.route('/getObstacles', methods=['GET'])
-#def getObstacles():
-    #global randomModel
+# This route will be used to get the positions of the obstacles
+@app.route('/getObstacles', methods=['GET'])
+@cross_origin()
+def getObstacles():
+    global randomModel
 
-    #if request.method == 'GET':
-        ## Get the positions of the obstacles and return them to Unity in JSON format.
-        ## Same as before, the positions are sent as a list of dictionaries, where each dictionary has the id and position of an obstacle.
-        #carPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for a, (x, z) in randomModel.grid.coord_iter() if isinstance(a, ObstacleAgent)]
+    if request.method == 'GET':
+        try:
+        # Get the positions of the obstacles and return them to WebGL in JSON.json.t.
+        # Same as before, the positions are sent as a list of dictionaries, where each dictionary has the id and position of an obstacle.
+            carPositions = [
+                {"id": str(a.unique_id), "x": x, "y":1, "z":z}
+                for a, (x, z) in randomModel.grid.coord_iter() if isinstance(a, ObstacleAgent)
+            ]
 
-        #return jsonify({'positions':carPositions})
+            return jsonify({'positions':carPositions})
+        except Exception as e:
+            print(e)
+            return jsonify({"message":"Error with obstacle positions"}), 500
 
-## This route will be used to update the model
-#@app.route('/update', methods=['GET'])
-#def updateModel():
-    #global currentStep, randomModel
-    #if request.method == 'GET':
-        ## Update the model and return a message to Unity saying that the model was updated successfully
-        #randomModel.step()
-        #currentStep += 1
-        #return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
+# This route will be used to update the model
+@app.route('/update', methods=['GET'])
+@cross_origin()
+def updateModel():
+    global currentStep, randomModel
+    if request.method == 'GET':
+        try:
+        # Update the model and return a message to WebGL saying that the model was updated successfully
+            randomModel.step()
+            currentStep += 1
+            return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
+        except Exception as e:
+            print(e)
+            return jsonify({"message":"Error during step."}), 500
+
 
 if __name__=='__main__':
     # Run the flask server in port 8585
